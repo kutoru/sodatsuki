406c8ab591fce4818f498161bfe59c0a723dd3e3
k <k>
2026-08-25 14:52:39 +0500

    add note field display

--- src/components/RightPanel.tsx
+++ src/components/RightPanel.tsx
@@ -1,9 +1,10 @@
-import { Ref, useEffect, useRef } from "react";
+import { Ref, useEffect, useState } from "react";
 import { Separator } from "./Separator";
 import { useStore } from "../hooks/useStore";
 import { Button } from "./Button";
-import { CheckIcon, RotateCwIcon, XIcon } from "lucide-react";
+import { CheckIcon, PencilIcon, RotateCwIcon, XIcon } from "lucide-react";
 import clsx from "clsx";
+import { Field, Note } from "../types";
 
 type Props = {
   rightPanel: Ref<HTMLDivElement>;
@@ -15,38 +16,53 @@ export const RightPanel = ({ rightPanel, rightResize, blurFilter }: Props) => {
   const currentNote = useStore((state) => state.currentNote);
   const setCurrentNote = useStore((state) => state.setCurrentNote);
 
-  const lastNoteExpression = useRef<string>("");
+  const [editNote, setEditNote] = useState<Note>();
 
   useEffect(() => {
-    if (currentNote?.fields.Expression) {
-      lastNoteExpression.current = currentNote.fields.Expression;
+    if (currentNote) {
+      setEditNote(structuredClone(currentNote));
     }
+
+    console.log("note", currentNote);
   }, [currentNote]);
+
+  const relevantFields: Field[] = [
+    "Meaning",
+    "Reading",
+    "Audio",
+    "Sentence",
+    "Sentence Audio",
+    "Image_URI",
+  ];
 
   return (
     <>
       <div
         ref={rightResize}
-        className="w-3 flex-none cursor-ew-resize select-none"
+        className="w-3 flex-none cursor-ew-resize overflow-auto select-none"
       />
 
       <div
         ref={rightPanel}
-        className="flex-1 bg-white/3 shadow-even shadow-black"
+        className="flex flex-1 flex-col overflow-auto bg-white/3 shadow-even shadow-black"
         style={blurFilter}
       >
         <div className="flex flex-row items-center">
-          <Button className="p-2.5" disabled={!currentNote}>
+          <Button
+            onClick={() => setEditNote(structuredClone(currentNote))}
+            className="p-2.5"
+            disabled={!currentNote}
+          >
             <RotateCwIcon className="size-full" />
           </Button>
 
           <div
             className={clsx(
-              "flex-1 overflow-hidden text-lg text-ellipsis whitespace-nowrap drop-shadow-even drop-shadow-black transition",
+              "flex-1 overflow-hidden text-lg text-ellipsis whitespace-nowrap drop-shadow-even drop-shadow-black transition-opacity",
               !currentNote && "opacity-0 select-none",
             )}
           >
-            {currentNote?.fields.Expression || lastNoteExpression.current}
+            {editNote?.fields.Expression}
           </div>
 
           <Button className="p-2" disabled={!currentNote}>
@@ -65,6 +81,45 @@ export const RightPanel = ({ rightPanel, rightResize, blurFilter }: Props) => {
         </div>
 
         <Separator />
+
+        <div
+          className={clsx(
+            "slim-scrollbar overflow-auto pb-2 transition-opacity",
+            !currentNote && "pointer-events-none opacity-0 select-none",
+          )}
+        >
+          {relevantFields.map((field) => (
+            <div key={field} className="flex flex-col">
+              <div className="flex flex-row items-center">
+                <div className="flex-1 overflow-hidden ps-2 text-ellipsis whitespace-nowrap drop-shadow-even drop-shadow-black">
+                  {field}
+                </div>
+
+                <Button className="p-2">
+                  <PencilIcon className="size-full" />
+                </Button>
+              </div>
+
+              <div
+                className={clsx(
+                  "mx-2 rounded-md bg-white/5 p-1 wrap-break-word shadow-even shadow-black/25",
+                  !editNote?.fields[field] && "text-gray-400/75 italic",
+                )}
+                dangerouslySetInnerHTML={{
+                  __html: editNote?.fields[field] || `${field}...`,
+                }}
+              />
+
+              {/* <div className="flex px-2">
+                <textarea
+                  className="h-32 w-0 flex-1 resize-none rounded-md bg-white/5 p-1 shadow-even shadow-black/25"
+                  value={currentNote?.fields[field]}
+                  disabled
+                />
+              </div> */}
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
