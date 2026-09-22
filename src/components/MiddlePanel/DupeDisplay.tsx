d49640236a2f1013781fb4d295611aedb3e8d9ed
k <k>
2026-09-22 13:01:58 +0900

    add dupe reload on anki refresh

--- src/components/MiddlePanel/DupeDisplay.tsx
+++ src/components/MiddlePanel/DupeDisplay.tsx
@@ -1,5 +1,5 @@
 import { useStore } from "../../hooks/useStore";
-import { DupeNote, NotificationType } from "../../types";
+import { DupeNote, NotificationType, Status } from "../../types";
 import { invoke } from "@tauri-apps/api/core";
 import { handleError } from "../../utils";
 import { Button } from "../Button";
@@ -9,12 +9,13 @@ import { Separator } from "../Separator";
 import clsx from "clsx";
 
 export const DupeDisplay = () => {
+  const anki = useStore((state) => state.anki);
   const ankiAddress = useStore((state) => state.ankiAddress);
   const showNotification = useStore((state) => state.showNotification);
   const selectedNote = useStore((state) => state.selectedNote);
 
   const [dupes, setDupes] = useState<DupeNote[]>([]);
-  const [currId, setCurrId] = useState<number>();
+  const [prevDupes, setPrevDupes] = useState<DupeNote[]>([]);
 
   const openDupesInAnki = () => {
     invoke("anki_open_notes", { ankiAddress, noteIds: dupes.map((v) => v.id) })
@@ -23,28 +24,25 @@ export const DupeDisplay = () => {
   };
 
   useEffect(() => {
-    if (!selectedNote) {
+    if (!selectedNote || anki.status !== Status.Online) {
       return;
     }
-
-    const newId = selectedNote.id;
 
     invoke<DupeNote[]>("anki_get_dupes", {
       ankiAddress,
       expression: selectedNote.fields["Expression"],
     })
       .then((dupes) => {
-        if (dupes.length <= 1) {
-          return;
-        }
-
         setDupes(dupes);
-        setCurrId(newId);
+
+        if (dupes.length > 1) {
+          setPrevDupes(dupes);
+        }
       })
       .catch(handleError());
-  }, [selectedNote]);
+  }, [selectedNote, anki]);
 
-  const show = !!selectedNote && selectedNote.id === currId && dupes.length > 1;
+  const show = dupes.length > 1;
 
   return (
     <>
@@ -65,8 +63,8 @@ export const DupeDisplay = () => {
           <div className="pointer-events-none absolute -bottom-0.5 h-0.5 w-full rounded-full bg-amber-500/90 shadow-even shadow-amber-500/90 transition group-hover:bg-amber-500 group-hover:shadow-amber-500 group-active:bg-amber-500/60 group-active:shadow-amber-500/60" />
 
           <span className="line-clamp-1 drop-shadow-even drop-shadow-black transition group-hover:drop-shadow-white/50 group-active:text-gray-400">
-            <span className="font-bold">{dupes.length}: </span>
-            {dupes.map((v) => v.deck).join(", ")}
+            <span className="font-bold">{prevDupes.length}: </span>
+            {prevDupes.map((v) => v.deck).join(", ")}
           </span>
         </a>
 
